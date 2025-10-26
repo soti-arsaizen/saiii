b562b27467cabcdc3554f345ec163c7677476cac
soti-arsaizen <soti-arsaizen@isufst.edu.ph>
2025-10-26 13:48:03 +0800

    app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-return "Welcome to my Flask API!"
+  return "Welcome to my Flask API!"
 
 @app.route('/student')
 def get_student():
-return jsonify({
+  return jsonify({
 "name": "Your Name",
 "grade": 10,
 "section": "Sai Zen Soti-ar"
